5d0ea06f4fd3f8986b3778932f45c26c90be4bc7
Christian Camilo Galeano <chcamilog@gmail.com>
2024-07-03 10:30:58 -0500

    ejercicio sección 5

---
+++ Ejercicios/seccion5/ejercicio.ts
@@ -0,0 +1,81 @@
+// Objetos
+
+type carro = {
+  carroceria: string,
+  modelo: string,
+  antibalas: boolean,
+  pasajeros: number,
+  disparar?: () => void
+  // disparar?(): void
+}
+
+const batimovil: carro = {
+  carroceria: "Negra",
+  modelo: "6x6",
+  antibalas: true,
+  pasajeros: 4
+};
+
+const bumblebee = {
+  carroceria: "Amarillo con negro",
+  modelo: "4x2",
+  antibalas: true,
+  pasajeros: 4,
+  disparar() { // El metodo disparar es opcional
+    console.log("Disparando");
+  }
+};
+
+
+// Villanos debe de ser un arreglo de objetos personalizados
+type villano = {
+  nombre: string,
+  edad?: number,
+  mutante: boolean
+}
+
+const villanos: villano[] = [{
+  nombre: "Lex Luthor",
+  edad: 54,
+  mutante: false
+}, {
+  nombre: "Erik Magnus Lehnsherr",
+  edad: 49,
+  mutante: true
+}, {
+  nombre: "James Logan",
+  edad: undefined,
+  mutante: true
+}];
+
+// Multiples tipos
+// cree dos tipos, uno para charles y otro para apocalipsis
+type mutante1 = {
+  poder: string,
+  estatura: number
+}
+const charles: mutante1 = {
+  poder: "psiquico",
+  estatura: 1.78
+};
+
+type mutante2 = {
+  lider: boolean,
+  miembros: string[]
+}
+
+const apocalipsis: mutante2 = {
+  lider: true,
+  miembros: ["Magneto", "Tormenta", "Psylocke", "Angel"]
+}
+
+// Mystique, debe poder ser cualquiera de esos dos mutantes (charles o apocalipsis)
+let mystique: mutante1 | mutante2;
+
+mystique = charles;
+mystique = apocalipsis;
+
+let auto = {
+  puertas: 10,
+  marca: 'Sedan'
+}
